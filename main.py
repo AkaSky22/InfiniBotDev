619dda64abfbd5a431b49205eea8e54010a76438
AkaSky22 <akasky.Voyager.22@gmail.com>
2024-05-26 20:03:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+# Meet Infinibot: your friend
 
-#Meet InfiniBot: your friend
-
-#import necessary libraries
+# import necessary libraries
 import io
 import random
-import string # to process standard python strings
+import string
 import warnings
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -14,20 +13,15 @@ warnings.filterwarnings('ignore')
 
 import nltk
 from nltk.stem import WordNetLemmatizer
-nltk.download('popular', quiet=True) # for downloading packages
+nltk.download('popular', quiet=True)
 
-# uncomment the following only the first time
-#nltk.download('punkt') # first-time use only
-#nltk.download('wordnet') # first-time use only
-
-
-#Reading in the corpus
-with open('chatbot.txt','r', encoding='utf8', errors ='ignore') as fin:
+# Reading in the corpus
+with open('chatbot.txt', 'r', encoding='utf8', errors='ignore') as fin:
     raw = fin.read().lower()
 
-#TOkenisation
-sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences 
-word_tokens = nltk.word_tokenize(raw)# converts to list of words
+# Tokenization
+sent_tokens = nltk.sent_tokenize(raw)
+word_tokens = nltk.word_tokenize(raw)
 
 # Preprocessing
 lemmer = WordNetLemmatizer()
@@ -37,10 +31,9 @@ remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
 def LemNormalize(text):
     return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
 
-
 # Keyword Matching
-GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey","How are You")
-GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
+GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up", "hey", "how are you")
+GREETING_RESPONSES = ["hello!", "hi!", "hey!", "hi there!", "hello! how can i help you?", "hi! what can i do for you?"]
 
 def greeting(sentence):
     """If user's input is a greeting, return a greeting response"""
@@ -48,44 +41,40 @@ def greeting(sentence):
         if word.lower() in GREETING_INPUTS:
             return random.choice(GREETING_RESPONSES)
 
-
 # Generating response
 def response(user_response):
-    InfiniBot_response=''
+    infinibot_response = ''
     sent_tokens.append(user_response)
     TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
     tfidf = TfidfVec.fit_transform(sent_tokens)
     vals = cosine_similarity(tfidf[-1], tfidf)
-    idx=vals.argsort()[0][-2]
+    idx = vals.argsort()[0][-2]
     flat = vals.flatten()
     flat.sort()
     req_tfidf = flat[-2]
-    if(req_tfidf==0):
-        InfiniBot_response=InfiniBot_response+"I am sorry! I don't understand you"
-        return InfiniBot_response
+    if req_tfidf == 0:
+        infinibot_response = infinibot_response + "I am sorry! I don't understand you."
+        return infinibot_response
     else:
-        InfiniBot_response = InfiniBot_response+sent_tokens[idx]
-        return InfiniBot_response
+        infinibot_response = infinibot_response + sent_tokens[idx]
+        return infinibot_response
 
-
-flag=True
-print("InfiniBot: My name is InfiniBot. I will answer your queries about Chatbots. If you want to exit, type Bye!")
-while(flag==True):
+flag = True
+print("INFINIBOT: My name is Infinibot. I will answer your queries about Chatbots. If you want to exit, type Bye!")
+while flag == True:
     user_response = input()
-    user_response=user_response.lower()
-    if(user_response!='bye'):
-        if(user_response=='thanks' or user_response=='thank you' ):
-            flag=False
-            print("InfiniBot: You are welcome..")
+    user_response = user_response.lower()
+    if user_response != 'bye':
+        if user_response == 'thanks' or user_response == 'thank you':
+            flag = False
+            print("INFINIBOT: You are welcome.")
         else:
-            if(greeting(user_response)!=None):
-                print("InfiniBot: "+greeting(user_response))
+            if greeting(user_response) is not None:
+                print("INFINIBOT: " + greeting(user_response))
             else:
-                print("InfiniBot: ",end="")
+                print("INFINIBOT: ", end="")
                 print(response(user_response))
                 sent_tokens.remove(user_response)
     else:
-        flag=False
-        print("InfiniBot: Bye! take care..")    
-        
-        
+        flag = False
+        print("INFINIBOT: Bye! Take care.")
